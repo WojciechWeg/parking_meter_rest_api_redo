83a277968185f9224a427e72d9dee51e285ab5bb
WojciechWeg <wojtek.weg@gmail.com>
2019-09-23 22:08:29 +0200

    calc tests

--- src/test/java/com/wojtek/parkingmeter/calc/DisableCalcTest.java
+++ src/test/java/com/wojtek/parkingmeter/calc/DisableCalcTest.java
@@ -1,17 +1,35 @@
 package com.wojtek.parkingmeter.calc;
 
+import com.wojtek.parkingmeter.helpers.calcs.DisabledCalc;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 public class DisableCalcTest {
 
-    @Test
-    public void lessThanHour(){}
+    DisabledCalc disabledCalc = new DisabledCalc();
 
     @Test
-    public void moreThanHourAndLessThanTwoHours(){}
+    public void lessThanHour(){
+        assertEquals("Less than hour: ", BigDecimal.valueOf(0).setScale(2),disabledCalc.calculate((long) 0.5));
+    }
 
     @Test
-    public void moreThanTwoHours(){}
+    public void moreThanHourAndLessThanTwoHours(){
+        assertEquals("More than hour and less than two hours: ", BigDecimal.valueOf(2).setScale(2),disabledCalc.calculate((long) 1.25));
+    }
+
+    @Test
+    public void moreThanTwoHours(){
+        assertEquals("More than two hours ",BigDecimal.valueOf(4.40).setScale(2),disabledCalc.calculate((long) 2.25));
+    }
+
+    @Test
+    public void moreThanThreeHours(){
+        assertEquals("More than three hours ",BigDecimal.valueOf(7.28).setScale(2),disabledCalc.calculate((long) 3.25));
+    }
 
 
 }
--- src/test/java/com/wojtek/parkingmeter/calc/RegularCalcTest.java
+++ src/test/java/com/wojtek/parkingmeter/calc/RegularCalcTest.java
@@ -1,16 +1,35 @@
 package com.wojtek.parkingmeter.calc;
 
+import com.wojtek.parkingmeter.helpers.calcs.RegularCalc;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
 
 public class RegularCalcTest {
 
-    @Test
-    public void lessThanHour(){}
+    RegularCalc regularCalc = new RegularCalc();
 
     @Test
-    public void moreThanHourAndLessThanTwoHours(){}
+    public void lessThanHour(){
+        assertEquals("Less than hour: ", BigDecimal.valueOf(1.00).setScale(2),regularCalc.calculate((long) 0.5));
+    }
 
     @Test
-    public void moreThanTwoHours(){}
+    public void moreThanHourAndLessThanTwoHours(){
+        assertEquals("More than hour and less than two hours: ",BigDecimal.valueOf(3.00).setScale(2),regularCalc.calculate((long) 1.25));
+    }
+
+    @Test
+    public void moreThanTwoHours(){
+        assertEquals("More than two hours ",BigDecimal.valueOf(6.00).setScale(2),regularCalc.calculate((long) 2.25));
+    }
+
+    @Test
+    public void moreThanThreeHours(){
+        assertEquals("More than three hours ",BigDecimal.valueOf(10.5).setScale(2),regularCalc.calculate((long) 3.25));
+    }
 
 }
