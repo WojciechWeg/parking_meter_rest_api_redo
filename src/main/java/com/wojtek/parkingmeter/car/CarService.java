7736e2db3ee807d07b452640b4110eb3316e57cc
WojciechWeg <wojtek.weg@gmail.com>
2019-09-24 16:50:37 +0200

    parking operator case done

--- src/main/java/com/wojtek/parkingmeter/car/CarService.java
+++ src/main/java/com/wojtek/parkingmeter/car/CarService.java
@@ -1,23 +1,45 @@
 package com.wojtek.parkingmeter.car;
 
 import com.wojtek.parkingmeter.helpers.Validator;
+import com.wojtek.parkingmeter.ticket.TicketRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CarService {
 
     private final Validator validator;
+    private final CarRepository carRepository;
+    private final TicketRepository ticketRepository;
 
-    public CarService( Validator validator) {
+    public CarService(Validator validator, CarRepository carRepository, TicketRepository ticketRepository) {
         this.validator = validator;
+        this.carRepository = carRepository;
+        this.ticketRepository = ticketRepository;
     }
-
 
     public boolean hasStarted(String numberPlate) {
 
         validator.checkNumberPlate(numberPlate);
 
-        return validator.hasStarted(numberPlate);
+        Optional<Integer> carID = carRepository.findIdByNrPlate(numberPlate);
 
+        if(carID.isPresent()) {
+            if(hasValidTicket(numberPlate)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean hasValidTicket(String numberPlate){
+        return ticketRepository.findByCarNumberPlateAndCarIdIsNull(numberPlate).isPresent();
     }
 }
--- src/main/java/com/wojtek/parkingmeter/ticket/TicketRepository.java
+++ src/main/java/com/wojtek/parkingmeter/ticket/TicketRepository.java
@@ -1,7 +1,13 @@
 package com.wojtek.parkingmeter.ticket;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface TicketRepository extends CrudRepository<TicketEntity, Long> {
+
+    @Query(value = "SELECT c.id FROM tickets c WHERE cars_id IS NOT null AND car_number_plate = ?1 ",nativeQuery = true)
+    Optional<Long> findByCarNumberPlateAndCarIdIsNull(String numberPlate);
 
 }
