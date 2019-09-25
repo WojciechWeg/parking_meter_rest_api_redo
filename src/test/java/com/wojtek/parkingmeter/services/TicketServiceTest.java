f7b7a61cde0bad13d2de637c55fb5ca99db94e09
WojciechWeg <wojtek.weg@gmail.com>
2019-09-25 12:37:40 +0200

    ticket service test - asked for help

--- src/test/java/com/wojtek/parkingmeter/services/TicketServiceTest.java
+++ src/test/java/com/wojtek/parkingmeter/services/TicketServiceTest.java
@@ -1,15 +1,33 @@
 package com.wojtek.parkingmeter.services;
 
 import com.wojtek.parkingmeter.car.CarRepository;
+import com.wojtek.parkingmeter.helpers.Validator;
+import com.wojtek.parkingmeter.helpers.calcs.ChargeCalculator;
 import com.wojtek.parkingmeter.profit.ProfitRepository;
-import com.wojtek.parkingmeter.ticket.TicketRepository;
-import com.wojtek.parkingmeter.ticket.TicketService;
+import com.wojtek.parkingmeter.ticket.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.time.LocalDateTime;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class TicketServiceTest {
 
+    Validator validator;
     TicketService ticketService;
+    TicketDTO ticketDTO;
+
+    @Mock
+    ChargeCalculator chargeCalculator;
+
+    @Mock
+    TicketMapper ticketMapper;
 
     @Mock
     private  TicketRepository ticketRepository;
@@ -20,9 +38,31 @@ public class TicketServiceTest {
     @Mock
     private  ProfitRepository profitRepository;
 
+    @BeforeEach
+    public void setUp(){
+        validator = new Validator(ticketRepository,carRepository);
+        ticketService = new TicketService(ticketRepository,carRepository,profitRepository,ticketMapper,chargeCalculator,validator);
+
+        ticketDTO = new TicketDTO();
+        ticketDTO.setCarNumberPlate("12345");
+        ticketDTO.setTicketType(TicketType.REGULAR);
+        ticketDTO.setId(1L);
+        ticketDTO.setStampStart(LocalDateTime.now());
+    }
 
     @Test
-    public void startValidTicket(){};
+    public void startValidTicket(){
+
+        when(ticketService.startTicket(any(),any())).thenReturn(ticketDTO);
+
+        TicketDTO  ticketDTOreturned = ticketService.startTicket(TicketType.REGULAR.toString(),"12345");
+
+        assertEquals(ticketDTO.getCarNumberPlate(),ticketDTOreturned.getCarNumberPlate());
+        assertEquals(ticketDTO.getTicketType(),ticketDTOreturned.getTicketType());
+        assertEquals(ticketDTO.getId(),ticketDTOreturned.getId());
+        assertEquals(ticketDTO.getStampStart(),ticketDTOreturned.getStampStart());
+
+    };
 
     @Test
     public void startTicketWithInvalidTicketType(){}
